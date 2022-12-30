846cabba6b755966a9408b51cc0f87acca5a08a5
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-12-29 17:13:25 -0700

    display budgets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container, Stack, Button } from "react-bootstrap";
+import AddBudgetModal from "./components/AddBudgetModal";
+import { BudgetCard } from "./components/BudgetCard";
+import { useBudgets } from "./context/BudgetContext";
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const { budgets } = useBudgets();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Container>
+        <Stack direction="horizontal" gap="2" className="my-4">
+          <h1 className="mx-auto">Budget</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplaterColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {budgets.map((budget) => {
+            console.log(budget);
+            return (
+              <BudgetCard
+                key={budget.id}
+                name={budget.name}
+                amount={budget.amount}
+                max={budget.max}
+              />
+            );
+          })}
+
+          {/* {console.log(budgets)} */}
+        </div>
+      </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+    </>
   );
 }
 
