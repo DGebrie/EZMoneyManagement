980ada97b58241284f36766c46097aedc1d92696
DGebrie <81825713+DGebrie@users.noreply.github.com>
2023-04-25 12:26:36 -0700

    update homepage when no budget present

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Container, Stack, Button } from "react-bootstrap";
+import { Checkmark } from "react-checkmark";
 import AddBudgetModal from "./components/AddBudgetModal";
 import AddExpenseModal from "./components/AddExpenseModal";
 import ViewExpensesModal from "./components/ViewExpensesModal";
@@ -7,19 +8,9 @@ import BudgetCard from "./components/BudgetCard";
 import TotalBudgetCard from "./components/TotalBudgetCard";
 import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard ";
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./context/BudgetContext";
-
+import Benefits from "./components/Benefits";
 import piggy from "./images/piggybank.png";
-
-// import { Button, Stack } from "react-bootstrap"
-// import Container from "react-bootstrap/Container"
-// import AddBudgetModal from "./components/AddBudgetModal"
-// import AddExpenseModal from "./components/AddExpenseModal"
-// import ViewExpensesModal from "./components/ViewExpensesModal"
-// import BudgetCard from "./components/BudgetCard"
-// import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard"
-// import TotalBudgetCard from "./components/TotalBudgetCard"
-// import { useState } from "react"
-// import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetsContext"
+import save from "./images/saveimg.svg";
 
 function App() {
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
@@ -35,72 +26,130 @@ function App() {
 
   return (
     <>
-      <Container className="my-4">
-        <Stack direction="horizontal" gap="2" className="mb-4">
-          <h1 className="me-auto">
-            <img
-              src={piggy}
-              alt="Piggy Bank"
-              height="30px"
-              style={{ marginRight: "10px" }}
+      <section className="bg-light overflow-hidden">
+        {/* if no info added yet, use this homepage */}
+        {/* Welcome page */}
+        {/* If theres already info stored use this */}
+        <Container className="my-4">
+          <Stack direction="horizontal" gap="2" className="mb-4">
+            <h1 className="me-auto">
+              <img
+                src={piggy}
+                alt="Piggy Bank"
+                height="30px"
+                style={{ marginRight: "10px" }}
+              />
+              Penny Pincher
+            </h1>
+            <Button
+              variant="primary"
+              onClick={() => setShowAddBudgetModal(true)}
+            >
+              Add Budget
+            </Button>
+            <Button variant="outline-primary" onClick={openAddExpenseModal}>
+              Add Expense
+            </Button>
+          </Stack>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+              gap: "1rem",
+              alignItems: "flex-start",
+            }}
+          >
+            {budgets.map((budget) => {
+              const amount = getBudgetExpenses(budget.id).reduce(
+                (total, expense) => total + expense.amount,
+                0
+              );
+              return (
+                <BudgetCard
+                  key={budget.id}
+                  name={budget.name}
+                  amount={amount}
+                  max={budget.max}
+                  onAddExpenseClick={() => openAddExpenseModal(budget.id)}
+                  onViewExpensesClick={() =>
+                    setViewExpensesModalBudgetId(budget.id)
+                  }
+                />
+              );
+            })}
+            <UncategorizedBudgetCard
+              onAddExpenseClick={openAddExpenseModal}
+              onViewExpensesClick={() =>
+                setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)
+              }
             />
-            Penny Pincher
-          </h1>
-          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
-            Add Budget
-          </Button>
-          <Button variant="outline-primary" onClick={openAddExpenseModal}>
-            Add Expense
-          </Button>
-        </Stack>
+            <TotalBudgetCard />
+          </div>
+        </Container>
+        <AddBudgetModal
+          show={showAddBudgetModal}
+          handleClose={() => setShowAddBudgetModal(false)}
+        />
+        <AddExpenseModal
+          show={showAddExpenseModal}
+          defaultBudgetId={addExpenseModalBudgetId}
+          handleClose={() => setShowAddExpenseModal(false)}
+        />
+        <ViewExpensesModal
+          budgetId={viewExpensesModalBudgetId}
+          handleClose={() => setViewExpensesModalBudgetId()}
+        />
+
+        {/* notification bar */}
         <div
+          className="text-white text-center py-1 w-100"
+          style={{ backgroundColor: "#2256e8" }}
+        >
+          <h6>
+            See why Penny Pincher is the #1 budgeting tool{" "}
+            <a href="" className="text-white">
+              See Reviews
+            </a>
+          </h6>
+        </div>
+
+        {/* Hero only to be shown if no budgets */}
+
+        <div
+          className="p-6 d-flex justify-content-center row"
           style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-            gap: "1rem",
-            alignItems: "flex-start",
+            backgroundColor: "#b3e5fc",
+            font: "#223344",
           }}
         >
-          {budgets.map((budget) => {
-            const amount = getBudgetExpenses(budget.id).reduce(
-              (total, expense) => total + expense.amount,
-              0
-            );
-            return (
-              <BudgetCard
-                key={budget.id}
-                name={budget.name}
-                amount={amount}
-                max={budget.max}
-                onAddExpenseClick={() => openAddExpenseModal(budget.id)}
-                onViewExpensesClick={() =>
-                  setViewExpensesModalBudgetId(budget.id)
-                }
-              />
-            );
-          })}
-          <UncategorizedBudgetCard
-            onAddExpenseClick={openAddExpenseModal}
-            onViewExpensesClick={() =>
-              setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)
-            }
-          />
-          <TotalBudgetCard />
+          <div
+            className="m-5 font-weight-bold col-md-3 align-self-center"
+            style={{ color: "#223344" }}
+          >
+            <h1 className="mb-3">Monitor your funds,</h1>
+            <h4 className="mb-3">Maximize your fun.</h4>
+            <button
+              className="btn btn-lg "
+              style={{
+                backgroundColor: "#b3e5fc",
+                borderColor: "#223344",
+                font: "#223344",
+              }}
+              href=""
+              role="button"
+            >
+              Start Budgeting
+            </button>
+          </div>
+          <div className="col-md-3 align-self-center m-5">
+            <img src={save} alt="Piggy Bank" height="300px" style={{}} />
+          </div>
         </div>
-      </Container>
-      <AddBudgetModal
-        show={showAddBudgetModal}
-        handleClose={() => setShowAddBudgetModal(false)}
-      />
-      <AddExpenseModal
-        show={showAddExpenseModal}
-        defaultBudgetId={addExpenseModalBudgetId}
-        handleClose={() => setShowAddExpenseModal(false)}
-      />
-      <ViewExpensesModal
-        budgetId={viewExpensesModalBudgetId}
-        handleClose={() => setViewExpensesModalBudgetId()}
-      />
+        {/* Notification Bar */}
+
+        {/* Benefits */}
+        <Benefits />
+      </section>
     </>
   );
 }
---
+++ src/components/Benefits.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Checkmark } from "react-checkmark";
+import { Container, Stack, Button } from "react-bootstrap";
+
+export default function Benefits() {
+  const benefitsListData = [
+    {
+      title: "Avoid Overspending",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Avoid Overspending",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Avoid Overspending",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Avoid Overspending",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Avoid Overspending",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+  ];
+
+  return (
+    <div className="p-5 bg-light">
+      <h2 className="text-center mb-5">Benefits of Budgeting </h2>
+
+      {benefitsListData.map((benefit) => (
+        <>
+          <div className="row d-flex justify-content-center mb-5">
+            <div className="col-md-3 align-items-center mr-0 mb-4">
+              <Checkmark size="xxLarge" color="#223344" />
+            </div>
+            <div className="col-md-3 mr-4">
+              <h1 className="text-center font-italic">{benefit.title}</h1>
+              <p>{benefit.text}</p>
+            </div>
+          </div>
+        </>
+      ))}
+      <div className="d-flex justify-content-center">
+        <Button>Start Budgeting</Button>
+      </div>
+    </div>
+  );
+}
