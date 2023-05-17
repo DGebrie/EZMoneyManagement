6d2979e65ce98901f4292ee64e7a60ac20f3801c
DGebrie <81825713+DGebrie@users.noreply.github.com>
2023-05-17 00:05:34 -0700

    footer create

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,33 @@
 import React from "react";
+import piggy from "../images/piggybank.png";
 
 const Footer = () => {
-  return <div className="m-4 p-4">Footer Data</div>;
+  return (
+    <div className="m-4 p-4 ">
+      <div className="col-md-4">
+        <ul className="list-unstyled ">
+          <li>Blogs</li>
+          <li>Contact</li>
+          <li>About US</li>
+        </ul>
+      </div>
+
+      <h1
+        className="me-auto font-family-impact d-flex justify-content-center"
+        style={{
+          fontFamily: "impact",
+        }}
+      >
+        <img
+          src={piggy}
+          alt="Piggy Bank"
+          height="30px"
+          style={{ marginRight: "10px" }}
+        />
+        <em>Penny Pincher</em>
+      </h1>
+    </div>
+  );
 };
 
 export default Footer;
